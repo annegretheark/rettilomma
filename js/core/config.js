82336ab922ae7a61340017e47943a9a37275dc1c
Anne Grethe <anne-grethe.knutsen@atea-no>
2026-05-29 23:19:30 +0200

    Legg til config for Vercel

---
+++ js/core/config.js
@@ -0,0 +1,7 @@
+window.supabaseUrl = "https://amsmdvjdmfvreueixsbv.supabase.co"
+window.supabaseKey = "sb_publishable_A4gGBB7DiiEnFuog5mFQ1A_o8rIwIsI"
+
+window.supabaseClient = supabase.createClient(
+  window.supabaseUrl,
+  window.supabaseKey
+);
