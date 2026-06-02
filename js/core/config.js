f05dab476b7526aa2c09742e27f6576bbf87e74f
Anne Grethe <anne-grethe.knutsen@atea-no>
2026-06-02 16:10:44 +0200

    Add config.js for Vercel deployment

---
+++ js/core/config.js
@@ -0,0 +1,9 @@
+window.SUPABASE_URL = "https://amsmdvjdmfvreueixsbv.supabase.co";
+
+window.SUPABASE_ANON_KEY =
+  "sb_publishable_A4gGBB7DiiEnFuog5mFQ1A_o8rIwIsI";
+
+window.supabaseClient = supabase.createClient(
+  window.SUPABASE_URL,
+  window.SUPABASE_ANON_KEY
+);
