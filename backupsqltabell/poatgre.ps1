ab6e41c9689f15bfa9f044a735e8f7f8dedea2ad
Anne Grethe <anne-grethe.knutsen@atea-no>
2026-07-04 15:07:04 +0200

    Fix hovslager demo

---
+++ backupsqltabell/poatgre.ps1
@@ -0,0 +1,5 @@
+﻿& "C:\Program Files\PostgreSQL\18\bin\pg_dump.exe" `
+  "https://pxlbrywowphkczkehmee.supabase.co" `
+  --schema-only `
+  --schema public `
+  -f "C:\Users\anneg\Documents\supabase-schema-backup.sql"
